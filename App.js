024282b831ac76a0b4f8ea07515a3189ab05041f
NicoRiccitelli29 <nriccitelli@udesa.edu.ar>
2021-05-28 17:14:38 -0300

    Base inicial hecha App.js
    Tambien agregue las carpetas de Screen

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { 
   StyleSheet, 
   Text,
@@ -6,31 +6,55 @@ import {
   ScrollView
   } from 'react-native';
 
-export default function App() {
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state={
+        contador: 0,
+        saludo: '',
+        persona:{
+          name:'',
+          lastname:''
+        }
+    }
+  }
+
+  componentDidMount(){
+    this.getStringStorage();
+    this.getObjectStorage();
+  }
+
+ 
+
+
+
+render(){
   return (
-    <View style={{flex:1, 
-                  flexDirection: 'row',
-        }}>
-      <View style={{backgroundColor:"red", height:100}}> 
+    <View style={{flex:1, flexDirection: 'row',}}>
+      <View style={{backgroundColor:"red", justifyContent:'center',height:100, flexDirection:"column"}}> 
         <Text>Header</Text>
       </View>
+     
       <View style={{flex:1,
                     flexDirection:'row',
                     justifyContent:'space-around',
                     alignContent:'space-around',
                     flexWrap:'wrap'
       }}>
-        <ScrollView>
-          <View style={{backgroundColor:"red", height:150, width:150}}></View>
+        
+          <View style={{backgroundColor:"pink", height:150, width:150}}></View>
+          <View style={{backgroundColor:"lightblue", height:150, width:150}}></View>
+          <View style={{backgroundColor:"yellow", height:150, width:150}}></View>
           <View style={{backgroundColor:"green", height:150, width:150}}></View>
-          <View style={{backgroundColor:"orange", height:150, width:150}}></View>
-          <View style={{backgroundColor:"blue", height:150, width:150}}></View>
           <View style={{backgroundColor:"yellow", height:150, width:150}}></View>
           <View style={{backgroundColor:"red", height:150, width:150}}></View>
-        </ScrollView>
+        
       </View>
 
     </View>
   );
 }
-
+}
