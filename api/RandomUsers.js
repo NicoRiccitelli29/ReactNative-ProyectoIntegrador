814106cc48778693e056bfc62c02d129d96a3e5e
NicoRiccitelli29 <nriccitelli@udesa.edu.ar>
2021-06-01 17:39:02 -0300

    Update RandomUsers.js

--- api/RandomUsers.js
+++ api/RandomUsers.js
@@ -1,4 +1,4 @@
-export function getUserData(){
+export async function getUserData(){
     try{
         let resultado = await fetch('https://randomuser.me/api?results=10');
         let json = await rasultado.json();
