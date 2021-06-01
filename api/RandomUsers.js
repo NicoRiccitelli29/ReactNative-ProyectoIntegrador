2c0498ae0e6908ca79ed6c7ca42c0ed579aa0bc0
NicoRiccitelli29 <nriccitelli@udesa.edu.ar>
2021-06-01 16:09:04 -0300

    Cree carpeta api + randomUsers

---
+++ api/RandomUsers.js
@@ -0,0 +1,9 @@
+export function getUserData(){
+    try{
+        let resultado = await fetch('https://randomuser.me/api?results=10');
+        let json = await rasultado.json();
+        return json.results;
+    }catch(e){
+        console.log("Error: "+ e);
+    }
+}
